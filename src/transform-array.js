b145d8658051cd799c82e15a7110b0c7f473c5b3
SweetSheldon <48868754+SweetSheldon@users.noreply.github.com>
2020-09-20 23:38:41 +0300

    feat: add  transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(array) {
+  let result =[];
+  var i=0;
+  let j=0;
+  for(i=0;i<array.length;i++){
+    if(array[i]=='--discard-prev'&&i!=0){
+      if(array[i-2]!='--discard-next'){
+      result[j-1]=array[i+1];i++;}
+      else{continue}}
+    else if(array[i]=='--double-prev'&&i!=0){
+      if(array[i-2]!='--discard-next'){
+      result[j]=array[i-1];
+      j++;}
+      else{continue}}
+    else if(array[i]=='--double-next'&&i!=(array.length-1)){
+
+      result[j]=array[i+1];
+      j++;}
+    else if(array[i]=='--discard-next'&&i!=(array.length-1)){
+
+      result[j]=array[i+2];
+      i++;j++;}
+    else if(array[i]=='--discard-prev'&&i==0){continue}
+    else if(array[i]=='--double-prev'&&i==0){continue}
+    else if(array[i]=='--double-next'&&i>=array.length-1){break}
+    else if(array[i]=='--discard-next'&&i>=array.length-1){break}
+    // else if(array[i+1]='undefined'&&i>=array.length-1){break}
+    else{result[j]=array[i];j++;}
+  }
+  let resultArr =[];i=0;
+  for(j=0;j<result.length;j++){
+    if(result[j]!='--discard-prev'&&result[j]!="--double-prev"&&result[j]!='--double-next'&&result[j]!='--discard-next'&&(typeof result[j])!="undefined"){
+      resultArr[i]=result[j];i++;
+    }
+  }
+  
+  return resultArr;
 };
