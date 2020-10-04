e461fe43c9ed317166af3c61e5eb0fb7a80457b1
SweetSheldon <sheldongamesandlp@gmail.com>
2020-10-04 18:03:37 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.directMode = isDirect;
+}
+  encrypt(message, key) {
+    if(((typeof message)!="string")||((typeof key)!="string")){throw Error}
+    var msgNoSpaces = message.replace(/\s/g, '');
+    // if(message.length<key.length){do{message+=message;}while(message.length<key.length);message = message.slice(0,key.length)}
+    if(key.length<msgNoSpaces.length){do{key+=key;}while(key.length<msgNoSpaces.length);key = key.slice(0,msgNoSpaces.length)}
+    var result =''
+    var num;var j=0;
+    for(i=0;i<message.length;i++){
+    
+      if(message.toUpperCase().charCodeAt(i)<65||message.toUpperCase().charCodeAt(i)>90){num =message[i]; result+=num;}
+      else{
+        num=message.toUpperCase().charCodeAt(i)+key.toUpperCase().charCodeAt(j)-130
+        result+=String.fromCharCode(num%26+65);j++;
+      }
+    }
+    return this.directMode? result : result.split('').reverse().join('')
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+    if(((typeof message)!="string")||((typeof key)!="string")){throw Error}
+    var msgNoSpaces = message.replace(/\s/g, '');
+    // if(message.length<key.length){do{message+=message;}while(message.length<key.length);message = message.slice(0,key.length)}
+    if(key.length<msgNoSpaces.length){do{key+=key;}while(key.length<msgNoSpaces.length);key = key.slice(0,msgNoSpaces.length)}
+    var result =''
+    var num;var j=0;
+    for(i=0;i<message.length;i++){
+    
+      if(message.toUpperCase().charCodeAt(i)<65||message.toUpperCase().charCodeAt(i)>90){num =message[i]; result+=num;}
+      else{
+        num=Math.abs(message.toUpperCase().charCodeAt(i)-key.toUpperCase().charCodeAt(j)+26)
+        result+=String.fromCharCode(num%26+65);j++;
+      }
+    }
+    return this.directMode? result : result.split('').reverse().join('')
   }
 }
 
